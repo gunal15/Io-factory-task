14fc8c23d022d16fc633d94a0f3bbdcd7f5749b1
GUNAL N <113163255+gunal15@users.noreply.github.com>
2023-06-23 23:34:08 +0530

    Add files via upload

---
+++ Water tank/index.js
@@ -0,0 +1,47 @@
+function processArray() {
+  const inputArrayString = document.getElementById("inputArray").value;
+  const input = inputArrayString.split(",").map(Number);
+
+  for (let i = 0; i < 70; i++) {
+    document.getElementById(
+      "container"
+    ).innerHTML += `<div class="grid-item" id="item-${parseInt(i / 10)}-${
+      i % 10
+    }"></div>`;
+  }
+
+  input.forEach((item, index) => {
+    if (item !== 0) {
+      for (let i = 6, count = item; count > 0; i--, count--) {
+        const element = document.getElementById(`item-${i}-${index}`);
+        element.style.backgroundColor = "yellow";
+      }
+    }
+  });
+
+  var item1 = 0;
+  var index1 = 0;
+  var item2 = 0;
+  var index2 = 0;
+  input.forEach((item, index) => {
+    if (item !== 0 && item1 === 0) {
+      item1 = item;
+      index1 = index;
+    } else if (item !== 0 && item1 !== 0) {
+      item2 = item;
+      index2 = index;
+    }
+    while (item1 !== 0 && item2 !== 0) {
+      var height = Math.min(item1, item2);
+      for (let i = index1 + 1; i < index2; i++) {
+        for (let j = 6; j > 6 - height; j--) {
+          document.getElementById(`item-${j}-${i}`).style.backgroundColor =
+            "blue";
+        }
+      }
+      item1 = item2;
+      index1 = index2;
+      item2 = 0;
+    }
+  });
+}
